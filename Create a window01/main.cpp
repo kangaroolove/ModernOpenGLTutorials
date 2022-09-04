154c1007b73bbdcfa5f6900001d8ac9eb06004b4
pangkuanxin <351477478@qq.com>
2022-09-04 19:20:14 +0800

    重新提交教程1

--- Create a window01/main.cpp
+++ Create a window01/main.cpp
@@ -1,32 +1,57 @@
-// 必须包含的头文件
-#include <GL/freeglut.h>
-// 声明回调函数
-void RenderSceneCB(void);
+#define GLEW_STATIC
+#include <GL/glew.h>
 
-void main(int argc, char** argv)
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
 {
-	// 初始化glut
-	glutInit(&argc, argv);
-	// 初始化glut的显示模式GLUT_DOUBLE代表为双缓冲，RGBA为颜色缓冲
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	// 初始化窗口的尺寸
-	glutInitWindowSize(1024, 768);
-	// 初始化窗口的位置
-	glutInitWindowPosition(100, 100);
-	// 设置窗口的标题
-	glutCreateWindow("Create a window");
-	// RenderSceneCB是一个回调函数，GLUT会在内部循环不断调用这个函数
-	glutDisplayFunc(RenderSceneCB);
+	// 初始化GLFW
+	glfwInit();
+	// 设置context的版本号
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// 核心渲染模式
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "HelloWindow", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// 使用现代化技术管理OpenGL指针
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	int width = 0;
+	int height = 0;
+	glfwGetFramebufferSize(window, &width, &height);
+	// 设置视口
+	glViewport(0, 0, width, height);
+
 	// 设置清屏颜色
-	glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-	// 进入主事件循环
-	glutMainLoop();
-}
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-void RenderSceneCB(void)
-{
-	// 以glClearColor设置的颜色进行清屏
-	glClear(GL_COLOR_BUFFER_BIT);
-	// 交换前后缓冲
-	glutSwapBuffers();
+	while (!glfwWindowShouldClose(window))
+	{
+		// 接受鼠标和键盘事件
+		glfwPollEvents();
+		// 渲染指令
+		// 以设置的清屏颜色进行清屏
+		glClear(GL_COLOR_BUFFER_BIT);
+		// 前后缓冲交换，刷新屏幕
+		glfwSwapBuffers(window);
+	}
+
+
+	return 0;
 }
