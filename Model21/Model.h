b190c6038c2ca7b2b6e3cde452c8269c402fc947
PangKuanXin <pang.kuanxin@ultrastmedtech.com>
2023-03-17 17:25:18 +0800

    Add code

---
+++ Model21/Model.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include "Mesh.h"
+#include "Shader.h"
+
+class aiMaterial;
+class aiMesh;
+class aiScene;
+class aiNode;
+
+class Model
+{
+public:
+    Model(const std::string& path, bool gamma = false);
+    ~Model();
+    void Draw(Shader &shader);
+
+    std::vector<Texture> textures_loaded;
+    std::vector<Mesh> meshes;
+    std::string directory;
+    bool gammaCorrection;
+private:
+    void loadModel(const std::string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+    unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma);
+};
+
